a2912d09801b395f12f08b70622c2d0543f19ee8
LEMONCOMMANDER <jtroop130@gmail.com>
2026-02-05 09:44:46 -0500

    adding ; to api.js

--- be/api.js
+++ be/api.js
@@ -3,22 +3,23 @@ import sql from './db.mts'
 import express from 'express'
 import cors from 'cors'
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // Adds headers: Access-Control-Allow-Origin: *
-app.use(cors())
+app.use(cors());
 
 app.get('/test', async (req, res) => {
     try {
-        const result = await sql(`select * from api_test where id = $1`, [1])
-        res.send(result)
+        const result = await sql(`select * from api_test where id = $1`, [1]);
+        res.send(result);
     } catch (error) {
-        res.status(500).send({ error: error.message })
+        console.error(error);
+        res.status(500).send({ error: error.message });
     }
 
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
