5891ed5533f7b4454a1ed0bd5671fa743910da41
LEMONCOMMANDER <jtroop130@gmail.com>
2026-05-11 16:48:20 -0400

    adding mockdata to the front end matching schema

---
+++ fe/mockData.js
@@ -0,0 +1,55 @@
+users = [
+    {
+        id: 1,
+        firstname: "Joseph",
+        lastname: "Test"
+    },
+    {
+        id: 2,
+        firstname: "Collin",
+        lastname: "Tester"
+    }
+]
+
+//todo in progress done
+
+tickets = [
+    {
+        id: 1,
+        assignee: 1, //user id
+        title: "test ticket 1",
+        dueDate: null,
+        ticket: "this the description of ticket 1",
+        status: "todo",
+        lastModified: "2026-05-11 10:00:00",
+        completedDate: null,
+        cancelled: false,
+        cancelledDate: null,
+    },
+
+    {
+        id: 2,
+        assignee: 2, //user id
+        title: "test ticket 2",
+        dueDate: "2026-05-21",
+        ticket: "this the description of ticket 2",
+        status: "in_progress",
+        lastModified: "2026-05-11 10:00:00",
+        completedDate: null,
+        cancelled: false,
+        cancelledDate: null,
+    },
+    {
+        id: 3,
+        assignee: 1, //user id
+        title: "test ticket 3",
+        dueDate: "2026-05-22",
+        ticket: "this the description of ticket 3",
+        status: "done",
+        lastModified: "2026-05-11 10:00:00",
+        completedDate: "2026-05-11 10:00:00",
+        cancelled: false,
+        cancelledDate: null,
+    }
+
+]
